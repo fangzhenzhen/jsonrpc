272c9dc43bddf54e462da78b96363926854c37da
方溱 <rd_fangzhen@yuzhua.com>
2021-08-14 11:05:16 +0800

    update

---
+++ src/JsonRpc/TestComposer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * TestComposer.php
+ * 文件描述
+ * Created on 2021/8/14 11:03
+ * Created by Fangzhen
+ */
+
+namespace fangzhen\rpcservice\src\JsonRpc;
+
+
+class TestComposer
+{
+    const AA = 123;
+
+}
